230024fea4234f20470df1944e1deba7ca945675
Michael Ruttenberg <mjruttenberg@gmail.com>
2019-08-25 00:33:33 +0100

    Add messageing to dreidl game

--- dreidl.rb
+++ dreidl.rb
@@ -7,45 +7,67 @@ player1tokens = tokens/2
 player2tokens = tokens/2
 pot = 0 # establish the kitty
 
+puts "Player 1's current tokens: #{player1tokens}"
+puts "Player 2's current tokens: #{player2tokens}"
+puts "In the pot: #{pot}\n\n"
+
+
 # loop through the players
 while player1tokens > 0 and player1tokens> 0
   if player1turn == true
-    puts "player 1's turn"
+    puts "---------------------\nPlayer 1's turn\n\n"
+
+    puts "You put 1 token in the pot to access play\n\n"
+    pot += 1 # put a token in the pot. Pay to play.
+    player1tokens -= 1 # pay to play
 
     # roll dice contraining 4 faces: N, G, H, S
       # roll N (nun): do nothing
       # roll G (gimmel): take the pot
       # roll H (hay): get half the pot/kitty
-      # roll S (shin): out a token in the kitty
+      # roll S (shin): put a token in the kitty
 
     # determine result
       # determine if player lost
 
-    tokens -=1
-    puts "Player 1 tokens left = #{tokens}"
-    if tokens <=0
+    player1tokens -=1
+    puts "Player 1 tokens left = #{player1tokens}"
+    if player1tokens <=0
       puts "You are out of tokens. You lose. Player 2 wins"
       break # end the while loop
     end
 
 
+    puts "\n\n***END OF PLAYER 1'S TURN***\n\n"
+    puts "Player 1's current total: #{player1tokens}"
+    puts "Player 2's current total: #{player2tokens}"
+    puts "In the pot: #{pot}\n\n"
+
     # switch player
     player1turn = !player1turn
 
   else
-    puts "player 2's turn"
-    currentplayer = "player 2"
+    puts "---------------------\nPlayer 2's turn\n\n"
 
-    # roll dice
+    # roll dice contraining 4 faces: N, G, H, S
+      # roll N (nun): do nothing
+      # roll G (gimmel): take the pot
+      # roll H (hay): get half the pot/kitty
+      # roll S (shin): put a token in the kitty
 
     # determine result
       # determine if player lost
     
-    puts "Player 2 tokens left = #{tokens}"
-    if tokens <=0
+    puts "Player 2 tokens left = #{player2tokens}"
+    if player2tokens <=0
       puts "You are out of tokens. You lose. Player 1 wins"
       break # end the while loop
     end
+
+    puts "\n\n***END OF PLAYER 2'S TURN***\n\n"
+    puts "Player 1's current total: #{player1tokens}"
+    puts "Player 2's current total: #{player2tokens}"
+    puts "In the pot: #{pot}\n\n"
 
     # switch player
     player1turn = !player1turn
