e87518c274b42cdf83d6ae0477ad2c412aa9fcf0
Michael Ruttenberg <mjruttenberg@gmail.com>
2019-08-25 21:06:53 +0100

    Add roll outcomes behaviour

--- dreidl.rb
+++ dreidl.rb
@@ -1,7 +1,7 @@
 # set up global variables
 player1turn = true
 tokens = 6 # must be an even number. Set low initially so we can evaluate results.
-sides = ["[N]un","[G]immel","[H]ay","[S]hin"]
+sides = ["n","g","h","s"]
 
 #assumes numberOfPlayers = 2
 player1tokens = tokens/2 
@@ -26,18 +26,38 @@ while player1tokens > 0 and player1tokens> 0
     # roll dice contraining 4 faces: N, G, H, S
     roll = rand(0..3)
     puts "\nYou rolled '#{sides[roll]}'\n\n"
+
+    # determine roll outcomes
+    case sides[roll]
       # roll N (nun): do nothing
+      when "n"
+        puts  "Nun. No action required\n\n"
       # roll G (gimmel): take the pot
-      # roll H (hay): get half the pot/kitty
+      when "g"
+        puts "Gimmel. You win the pot of #{pot} tokens\n\n"
+        player1tokens += pot
+        pot = 0
+      # roll H (hay): get half the pot/kitty. If the pot has an odd number, you win half + 1 tokens
+      when "h"
+        if pot % 2 > 0
+          puts "Hay. You win half the pot, so you win #{(pot/2)+1} token(s)\n\n"
+          player1tokens += (pot/2)+1
+          pot -= (pot/2)+1
+        else
+          puts "Hay. You win half the pot, so you win #{pot/2} token(s)\n\n"
+          player1tokens += pot/2
+          pot -= pot/2
+        end
       # roll S (shin): put a token in the kitty
+      when "s"
+        puts "Shin. Put a token into the pot\n\n"
+        player1tokens -= 1
+        pot += 1
+    end
 
-    # determine result
-      # determine if player lost
-
-    player1tokens -=1
-    puts "Player 1 tokens left = #{player1tokens}"
+    # determine if player lost
     if player1tokens <=0
-      puts "You are out of tokens. You lose. Player 2 wins"
+      puts "Oy vey! You are out of tokens. YOU LOSE. ***Player 2 wins!***"
       break # end the while loop
     end
 
@@ -54,17 +74,40 @@ while player1tokens > 0 and player1tokens> 0
     puts "---------------------\nPlayer 2's turn\n\n"
 
     # roll dice contraining 4 faces: N, G, H, S
-      # roll N (nun): do nothing
-      # roll G (gimmel): take the pot
-      # roll H (hay): get half the pot/kitty
-      # roll S (shin): put a token in the kitty
-
-    # determine result
-      # determine if player lost
+    roll = rand(0..3)
+    puts "\nYou rolled '#{sides[roll]}'\n\n"
     
-    puts "Player 2 tokens left = #{player2tokens}"
+    # determine roll outcomes
+    case sides[roll]
+      # roll N (nun): do nothing
+      when "n"
+        puts  "Nun. No action required\n\n"
+      # roll G (gimmel): take the pot
+      when "g"
+        puts "Gimmel. You win the pot of #{pot} tokens\n\n"
+        player2tokens += pot
+        pot = 0
+      # roll H (hay): get half the pot/kitty. If the pot has an odd number, you win half + 1 tokens
+      when "h"
+        if pot % 2 > 0
+          puts "Hay. You win half the pot, so you win #{(pot/2)+1} token(s)\n\n"
+          player2tokens += (pot/2)+1
+          pot -= (pot/2)+1
+        else
+          puts "Hay. You win half the pot, so you win #{pot/2} token(s)\n\n"
+          player2tokens += pot/2
+          pot -= pot/2
+        end
+      # roll S (shin): put a token in the kitty
+      when "s"
+        puts "Shin. Put a token into the pot\n\n"
+        player2tokens -= 1
+        pot += 1
+    end
+
+    # determine if player lost
     if player2tokens <=0
-      puts "You are out of tokens. You lose. Player 1 wins"
+      puts "Oy vey! You are out of tokens. YOU LOSE. ***Player 1 wins!***"
       break # end the while loop
     end
 
